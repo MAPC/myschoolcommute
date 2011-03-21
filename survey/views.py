fe17ab7835b82fbf93e47c398c7d52bd70c88f64
Christian Spanring <cspanring@gmail.com>
2011-03-21 18:07:05 -0400

    added school url and view

--- survey/views.py
+++ survey/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.conf import settings
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.forms import ModelForm
+from survey.models import School
+
+from django.template import RequestContext
+
+def school(request, school_slug):
+    
+    school = School.objects.get(slug__iexact=school_slug)
+    
+    return HttpResponse("Schoolname: %s location: %s" % (school.name, school.location))
--- urls.py
+++ urls.py
@@ -7,6 +7,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Example:
     # (r'^walkboston/', include('walkboston.foo.urls')),
+    
+    (r'^(?P<school_slug>[-\w]+)/$', 'survey.views.school'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
