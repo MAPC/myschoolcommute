66ede5580bd82fe7b5c97b4f425fa600d28620c9
Christian Spanring <cspanring@gmail.com>
2011-03-22 13:23:04 -0400

    first stab at template (incl. Google Map)

--- survey/views.py
+++ survey/views.py
@@ -1,13 +1,18 @@
 from django.conf import settings
+from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.forms import ModelForm
-from survey.models import School
+from survey.models import School, Letter
 
 from django.template import RequestContext
 
-def school(request, school_slug):
+def index(request, school_slug):
     
     school = School.objects.get(slug__iexact=school_slug)
+    letter = Letter.objects.get(pk=1) # only one letter
     
-    return HttpResponse("Schoolname: %s location: %s" % (school.name, school.location))
+    return render_to_response('survey/index.html', 
+                              {'school' : school, 
+                               'letter' : letter}, 
+                              context_instance=RequestContext(request))
--- urls.py
+++ urls.py
@@ -7,12 +7,13 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Example:
     # (r'^walkboston/', include('walkboston.foo.urls')),
-    
-    (r'^(?P<school_slug>[-\w]+)/$', 'survey.views.school'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    
+    # school
+    (r'^(?P<school_slug>[-\w]+)/$', 'survey.views.index'),
 )
