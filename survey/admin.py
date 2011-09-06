6b0fab4f14e06cdc398646aa33c32717e6fe9c3f
Christian Spanring <cspanring@gmail.com>
2011-09-06 17:33:29 -0400

    improved admin

--- survey/admin.py
+++ survey/admin.py
@@ -2,28 +2,28 @@ from walkboston.survey.models import School, Survey, Child, District
 # from django.contrib import admin
 from django.contrib.gis import admin
 
-class DistrictAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}
+class DistrictAdmin(admin.OSMGeoAdmin):
+    prepopulated_fields = {'slug': ('distname',)}
 
-class SchoolAdmin(admin.GeoModelAdmin):
+class SchoolAdmin(admin.OSMGeoAdmin):
     fieldsets = [
         (None, 
-            {'fields': ['name', 'slug', 'survey_active', 'district', 'survey_incentive']}),
+            {'fields': ['name', 'slug', 'survey_active', 'districtid', 'survey_incentive']}),
         ('School Database Attributes', 
-            {'fields': ['school_id', 'address', 'municipality', 'state', 'zip', 'principal', 'phone', 'fax', 'grades', 'type']}),
+            {'fields': ['schid', 'address', 'town', 'state', 'zip', 'principal', 'phone', 'fax', 'grades', 'schl_type']}),
         ('Map',
-            {'fields': ['location', ]}),
+            {'fields': ['geometry', ]}),
     ]    
     list_filter = ['survey_active']
-    list_display = ('name', 'survey_count', 'municipality', 'grades', 'principal','phone',)
-    search_fields = ['name', 'municipality']
-    ordering = ['municipality']
+    list_display = ('name', 'survey_count', 'town', 'grades', 'principal','phone',)
+    search_fields = ['name', 'districtid__distname']
+    ordering = ['districtid__distname']
     prepopulated_fields = {'slug': ('name',)}
     
     def survey_count(self, obj):
         return obj.survey_set.count()
     
-class SurveyAdmin(admin.GeoModelAdmin):
+class SurveyAdmin(admin.OSMGeoAdmin):
     list_display = ('pk','school')
     search_fields = ['school__name']
 
