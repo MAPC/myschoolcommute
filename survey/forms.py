10a7cb2f3bb3ffec02af6cefbd3ae13e9843bac0
Pavel Dorovskoy <pavel@yellow.appliedgeosolutions.com>
2013-05-31 16:54:40 -0400

    PEP8 E126 Lint reformatting

--- survey/forms.py
+++ survey/forms.py
@@ -9,54 +9,74 @@ from survey.models import Survey, Child, School, CHILD_MODES, CHILD_GRADES, CHIL
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Field, Layout, Fieldset, ButtonHolder, Submit
 
+
 class SurveyForm(ModelForm):
     """
     Parent Survey Form for collecting basic household information.
     """
-    street = CharField(label=_('Name of your street'),
-                    widget = TextInput(attrs={'size': '30'}),
-                    required=False,)
-    cross_st = CharField(label=_('Name of nearest cross-street'),
-                    widget = TextInput(attrs={'size': '30'}),
-                    required=False,)
-    nr_vehicles = IntegerField(label=_('How many vehicles do you have in your household?'), 
-                    widget = TextInput(attrs={'size': '2'}),
-                    required=False,)
-    nr_licenses = IntegerField(label=_('How many people in your household have a driver\'s license?'),
-                    widget = TextInput(attrs={'size': '2'}),
-                    required=False,)
+    street = CharField(
+        label=_('Name of your street'),
+        widget=TextInput(attrs={'size': '30'}),
+        required=False,
+    )
+    cross_st = CharField(
+        label=_('Name of nearest cross-street'),
+        widget=TextInput(attrs={'size': '30'}),
+        required=False,
+    )
+    nr_vehicles = IntegerField(
+        label=_('How many vehicles do you have in your household?'),
+        widget=TextInput(attrs={'size': '2'}),
+        required=False,
+    )
+    nr_licenses = IntegerField(
+        label=_('How many people in your household have a driver\'s license?'),
+        widget=TextInput(attrs={'size': '2'}),
+        required=False,
+    )
 
     class Meta:
         model = Survey
         exclude = ('school', 'ip', 'created', 'modified', 'distance')
-        
+
         widgets = {
-            'location': HiddenInput(),         
+            'location': HiddenInput(),
         }
+
 
 class ChildForm(ModelForm):
     """
     Sub-form for collecting information for each child in given school.
     """
-    grade = ChoiceField(label=_('What grade is your child in?'),
-                      choices=CHILD_GRADES,
-                      required=True,
-                      initial='',)
-    to_school = ChoiceField(label=_('How does your child get TO school on most days?'),
-                      choices=CHILD_MODES,
-                      required=True,)
-    dropoff = ChoiceField(label=_('Do you usually drop off your child on your way to work or another destination?'),
-                      choices=CHILD_DROPOFF,
-                      required=False,
-                      initial='',)
-    from_school = ChoiceField(label=_('How does your child get home FROM school on most days?'),
-                      choices=CHILD_MODES,
-                      required=True,)
-    pickup = ChoiceField(label=_('Do you usually pick up your child on your way from work or another origin?'),
-                      choices=CHILD_DROPOFF,
-                      required=False,
-                      initial='',)
-    
+    grade = ChoiceField(
+        label=_('What grade is your child in?'),
+        choices=CHILD_GRADES,
+        required=True,
+        initial='',
+    )
+    to_school = ChoiceField(
+        label=_('How does your child get TO school on most days?'),
+        choices=CHILD_MODES,
+        required=True,
+    )
+    dropoff = ChoiceField(
+        label=_('Do you usually drop off your child on your way to work or another destination?'),
+        choices=CHILD_DROPOFF,
+        required=False,
+        initial='',
+    )
+    from_school = ChoiceField(
+        label=_('How does your child get home FROM school on most days?'),
+        choices=CHILD_MODES,
+        required=True,
+    )
+    pickup = ChoiceField(
+        label=_('Do you usually pick up your child on your way from work or another origin?'),
+        choices=CHILD_DROPOFF,
+        required=False,
+        initial='',
+    )
+
     class Meta:
         model = Child
 
@@ -65,11 +85,13 @@ YES_OR_NO = (
     (False, 'No')
 )
 
+
 class SchoolForm(ModelForm):
     survey_active = forms.TypedChoiceField(
         choices=YES_OR_NO, widget=forms.RadioSelect, coerce=bool, label="Is survey active?"
     )
     survey_incentive = forms.CharField(label="Survey Form Message")
+
     def __init__(self, *args, **kwargs):
         super(SchoolForm, self).__init__(*args, **kwargs)
 
@@ -81,9 +103,10 @@ class SchoolForm(ModelForm):
         model = School
         fields = ("name", "survey_active", "survey_incentive", )
 
+
 class ReportForm(forms.Form):
-    start_date = forms.DateField(widget=forms.DateInput(format = '%m/%d/%Y'), input_formats=('%m/%d/%Y',))
-    end_date = forms.DateField(widget=forms.DateInput(format = '%m/%d/%Y'), input_formats=('%m/%d/%Y',))
+    start_date = forms.DateField(widget=forms.DateInput(format='%m/%d/%Y'), input_formats=('%m/%d/%Y',))
+    end_date = forms.DateField(widget=forms.DateInput(format='%m/%d/%Y'), input_formats=('%m/%d/%Y',))
 
     def __init__(self, *args, **kwargs):
         super(ReportForm, self).__init__(*args, **kwargs)
