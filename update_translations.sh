06884097b699a54973bf1fde90e7bb38fc6148f0
Pavel Dorovskoy <pavel@yellow.appliedgeosolutions.com>
2013-07-09 18:33:01 -0400

    Added autotranslate script

---
+++ update_translations.sh
@@ -0,0 +1,15 @@
+cd survey
+../manage.py makemessages -l es
+../manage.py makemessages -l pt
+../manage.py makemessages -l fr
+../manage.py makemessages -l ht
+../manage.py makemessages -l vi
+../manage.py makemessages -l km
+../manage.py makemessages -l zh
+../manage.py makemessages -l ar
+find -name *.po -exec sed -i 's/\\u003c/</g' {} \;
+find -name *.po -exec sed -i 's/\\u003e/>/g' {} \;
+cd ..
+./autotranslate.py
+cd survey
+../manage.py compilemessages
