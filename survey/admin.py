7033501329f85eaf49e942bbc497218e1971c52e
Pavel Dorovskoy <pavel@yellow.appliedgeosolutions.com>
2013-08-07 16:45:21 -0400

    Added columns to admin

--- survey/admin.py
+++ survey/admin.py
@@ -2,33 +2,39 @@ from survey.models import School, Survey, Child, District
 # from django.contrib import admin
 from django.contrib.gis import admin
 
+
 class DistrictAdmin(admin.OSMGeoAdmin):
     prepopulated_fields = {'slug': ('distname',)}
 
+
 class SchoolAdmin(admin.OSMGeoAdmin):
     fieldsets = [
-        (None, 
+        (None,
             {'fields': ['name', 'slug', 'survey_active', 'districtid', 'survey_incentive']}),
-        ('School Database Attributes', 
+        ('School Database Attributes',
             {'fields': ['schid', 'address', 'town', 'state', 'zip', 'principal', 'phone', 'fax', 'grades', 'schl_type']}),
         ('Map',
             {'fields': ['geometry', ]}),
-    ]    
+    ]
     list_filter = ['survey_active']
-    list_display = ('name', 'survey_count', 'town', 'grades', 'principal','phone',)
+    list_display = ('name', 'survey_count', 'town', 'grades', 'principal', 'phone',)
     search_fields = ['name', 'districtid__distname']
     ordering = ['districtid__distname']
     prepopulated_fields = {'slug': ('name',)}
-    
+
     def survey_count(self, obj):
         return obj.survey_set.count()
-    
+
+
 class SurveyAdmin(admin.OSMGeoAdmin):
-    list_display = ('pk','school')
-    search_fields = ['school__name']
+    list_display = ('pk', 'school', 'location', 'distance', 'created',)
+    search_fields = ['school__name', 'street', 'cross_st']
+    ordering = ['created', 'school__name']
+
 
 class ChildAdmin(admin.ModelAdmin):
-    list_display = ('pk','survey')
+    list_display = ('pk', 'survey')
+
 
 admin.site.register(District, DistrictAdmin)
 admin.site.register(School, SchoolAdmin)
