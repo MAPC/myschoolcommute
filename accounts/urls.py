e281d904e873f15a51b7670451609341d7f56977
Pavel Dorovskoy <pavel@yellow.appliedgeosolutions.com>
2013-12-11 16:26:57 -0500

    Fixed url names, issue #50

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,18 +9,18 @@ urlpatterns = patterns('',
     url(r'^$', index, name='accounts'),
 
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html', 'authentication_form': LoginForm}, name='login'),
-    (r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'accounts/logout.html'}),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'accounts/logout.html'}, name="logout"),
 
     url(r'^register/$', register, name='register'),
     url(r'^register/success/$', register_success, name='register_success'),
 
-    (r'^password/change/$', 'django.contrib.auth.views.password_change', {'template_name': 'accounts/password_change_form.html'}),
-    (r'^password/done/$', 'django.contrib.auth.views.password_change_done', {'template_name': 'accounts/password_change_done.html'}),
+    url(r'^password/change/$', 'django.contrib.auth.views.password_change', {'template_name': 'accounts/password_change_form.html'}, name='password_change'),
+    url(r'^password/done/$', 'django.contrib.auth.views.password_change_done', {'template_name': 'accounts/password_change_done.html'}, name='passwork_change_done'),
 
-    (r'^reset/$', 'django.contrib.auth.views.password_reset', {'template_name': 'accounts/password_reset_form.html'}),
-    (r'^reset/done/$', 'django.contrib.auth.views.password_reset_done', {'template_name': 'accounts/password_reset_done.html'}),
-    (r'^reset/token/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'template_name': 'accounts/password_reset_confirm.html'}),
-    (r'^reset/complete/$', 'django.contrib.auth.views.password_reset_complete', {'template_name': 'accounts/password_reset_complete.html'}),
+    url(r'^reset/$', 'django.contrib.auth.views.password_reset', {'template_name': 'accounts/password_reset_form.html'}, name='password_reset'),
+    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_done', {'template_name': 'accounts/password_reset_done.html'}, name='password_reset_done'),
+    url(r'^reset/token/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'template_name': 'accounts/password_reset_confirm.html'}, name='password_reset_confirm'),
+    url(r'^reset/complete/$', 'django.contrib.auth.views.password_reset_complete', {'template_name': 'accounts/password_reset_complete.html'}, name='password_reset_complete'),
 
     (r'^profile/$', profile_authed),
     url(r'^edit/$', profile_edit),
