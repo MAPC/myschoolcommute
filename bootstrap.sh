30bbe4fb4e447d300028420a5417ab62f961bf54
allthesignals <wilburnforce@gmail.com>
2015-02-08 21:04:26 -0500

    Vagrant provisioning now includes pgrouting extensions.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,12 @@ apt-get update -qq
 apt-get install -y python-software-properties
 apt-add-repository -y ppa:ubuntugis/ppa
 add-apt-repository ppa:chris-lea/node.js
+apt-get install -y postgresql-9.1-postgis-2.0
+add-apt-repository -y ppa:georepublic/pgrouting
 apt-get update -qq
+apt-get install -y postgresql-9.1-pgrouting
 apt-get install -y \
         python-pip \
-        postgresql-9.1-postgis-2.0 \
         python-psycopg2 \
         libpq-dev \
         libjpeg-dev \
@@ -30,7 +32,3 @@ cd virtualenv
 source ./bin/activate
 pip install -r ../myschoolcommute/requirements.txt
 cd ..
-
-# cd myschoolcommute
-# . ../virtualenv/bin/activate
-# pip install -r requirements.txt
